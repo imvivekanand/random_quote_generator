4ee937f4b94432929d7a5e7d2955cbf7b0ee6ce6
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2024-01-07 11:56:10 +0530

    App is working

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const quoteContainer = document.getElementById('quote-container')
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [ ];
 
@@ -6,7 +10,21 @@ let apiQuotes = [ ];
 function newQuote( ){
     //Picking a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random( ) *  apiQuotes.length)];
-    console.log(quote);
+
+    //Checking if author field is blank and if it is blank replace it with 'Unknown';
+    if(!quote.author){
+        authorText.textContent = 'Unknown'
+    }else{
+    authorText.textContent = quote.author;
+    }
+
+    // Check quote length to determine styling
+    if(quote.text.length > 50){
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -20,6 +38,16 @@ async function getQuotes( ){
         console.log("No Data From The Jokes API: ", error);
     }
 }
+
+//Tweet Quote
+function tweetQuote( ){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote)
 
 
 
